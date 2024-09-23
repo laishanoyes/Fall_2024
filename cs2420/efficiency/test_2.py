6e69602d45a4efa23a166349d4846c761ef6308c
laishanoyes <d00447258@utahtech.edu>
2024-09-22 23:31:23 -0600

    testing

--- cs2420/efficiency/test_2.py
+++ cs2420/efficiency/test_2.py
@@ -1,8 +1,42 @@
-import numpy as np
+import random
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
+from sorting_algorithms import (
+    bubble_sort,
+    shaker_sort,
+    counting_sort,
+    merge_sort,
+    quick_sort,
+    mod_quick_sort
+)
 
-# ... (Your sorting functions and data generation functions remain unchanged)
+# Generate random data
+def make_random_data(size):
+    return [random.randint(0, 1000) for _ in range(size)]
+
+# Generate mostly sorted data
+def make_mostly_sorted_data(size):
+    data = make_random_data(size)
+    data.sort()
+    data[0], data[-1] = data[-1], data[0]  # Swap first and last
+    return data
+
+# Test all sorting algorithms
+def test_sorting_algorithms(data_generator, sizes):
+    results = {}
+    for size in sizes:
+        data = data_generator(size)
+        comparisons = {
+            "BubbleSort": bubble_sort(data.copy()),
+            "ShakerSort": shaker_sort(data.copy()),
+            "CountingSort": counting_sort(data.copy()),
+            "MergeSort": merge_sort(data.copy()),
+            "QuickSort": quick_sort(data.copy(), 0, size - 1),
+            "ModQuickSort": mod_quick_sort(data.copy(), 0, size - 1),
+        }
+        results[size] = comparisons
+    return results
 
 def run_tests_and_plot():
     sizes = [2**i for i in range(3, 12)]  # From 8 to 2048
@@ -45,4 +79,3 @@ def run_tests_and_plot():
     mostly_sorted_df_log.to_excel("mostly_sorted_data_comparisons_log.xlsx", index=True)
 
 run_tests_and_plot()
-
